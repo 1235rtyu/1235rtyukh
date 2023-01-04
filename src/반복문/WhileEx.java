7bbe8884ebc84228897ce0136d18e30823ec083d
mh <ch07124@naevr.com>
2023-01-04 14:55:25 +0900

    나무찍기

---
+++ src/반복문/WhileEx.java
@@ -0,0 +1,15 @@
+package 반복문;
+
+public class WhileEx {
+    public static void main(String[] args) {
+        int treeHit = 0;
+        while (true){
+            treeHit++;
+                    System.out.println("나무ㄷ를0" + treeHit + "번 찍었습니다");
+                    if(treeHit == 10) {
+                        System.out.println("나무가 넘어 갑니다");
+                        break;
+                    }
+        }
+    }
+}
